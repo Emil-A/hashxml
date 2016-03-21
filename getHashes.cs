85f2ca8c3b7a19c4b2d11705a66fe14588d70282
Emil Aite <emil.a.aite@hrsdc-rhdcc.gc.ca>
2016-03-21 15:44:20 -0400

    Updated all files into xml hash 4.0.0+

--- getHashes.cs
+++ getHashes.cs
@@ -1,29 +1,10 @@
 using System;
 using System.Xml;
 using System.IO;
+using System.Collections.Generic;
 
 public static class UpdateXml
 {
-	public static void getFiles(string path, string theme)
-	{
-		string fileText;
-		string[] links = new string[13] {
-		path+"/"+theme+"/css/theme.min.css",
-		path+"/wet-boew/js/wet-boew.min.js",
-		path+"/"+theme+"/js/theme.min.js",
-		path+"/wet-boew/js/polyfills/details.min.js",
-		path+"/wet-boew/css/polyfills/details.min.css",
-		path+"/wet-boew/js/polyfills/datepicker.min.js",
-		path+"/wet-boew/css/polyfills/datepicker.min.css",
-		path+"/wet-boew/js/i18n/en.min.js",
-		path+"/wet-boew/js/polyfills/details.min.js",
-		path+"/wet-boew/css/polyfills/details.min.css",
-		path+"/wet-boew/js/polyfills/datepicker.min.js",
-		path+"/wet-boew/css/polyfills/datepicker.min.css",
-		path+"/wet-boew/js/i18n/en.min.js"};
-		
-		 fileText = File.ReadAllText(@"");
-	}
 	public static int getHash(string str)
 	{
 		// function borrowed from http://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript-jquery
@@ -52,23 +33,56 @@ public static class UpdateXml
 		string fileText;
 		string fileName;
 		string dirName;
+		string fileLocation = "./test.xml";
 		int hash;
-		string[] links = new string[8] {
-		path+"/"+theme+"/css/theme.min.css",
-		path+"/wet-boew/js/wet-boew.min.js",
-		path+"/"+theme+"/js/theme.min.js",
-		path+"/wet-boew/js/polyfills/details.min.js",
-		path+"/wet-boew/css/polyfills/details.min.css",
-		path+"/wet-boew/js/polyfills/datepicker.min.js",
-		path+"/wet-boew/css/polyfills/datepicker.min.css",
-		path+"/wet-boew/js/i18n/en.min.js"};
+		List<string> links = new List<string>();
+		//Versions 15+
+		links.Add(path+"/"+theme+"/css/theme.min.css");
+		links.Add(path+"/wet-boew/js/wet-boew.min.js");
+		links.Add(path+"/"+theme+"/js/theme.min.js");
+		links.Add(path+"/wet-boew/js/polyfills/details.min.js");
+		links.Add(path+"/wet-boew/css/polyfills/details.min.css");
+		links.Add(path+"/wet-boew/js/polyfills/datepicker.min.js");
+		links.Add(path+"/wet-boew/css/polyfills/datepicker.min.css");
+		links.Add(path+"/wet-boew/js/i18n/en.min.js");
+		links.Add(path+"/"+theme+"/css/wet-boew.min.css");//only found in canada.ca
+		links.Add(path+"/"+theme+"/css/theme.css");//only found in canada.ca
 		
-		
+		//Versions 14-
+		/*links.Add(path+"/css/theme.min.css");
+		links.Add(path+"/js/wet-boew.min.js");
+		links.Add(path+"/js/theme.min.js");//not in v4.0.6 gcweb or below, not in v4.0.4 intranet below, not v4.0.3 ut below
+		links.Add(path+"/js/polyfills/details.min.js");
+		links.Add(path+"/css/polyfills/details.min.css");
+		links.Add(path+"/js/polyfills/datepicker.min.js");
+		links.Add(path+"/css/polyfills/datepicker.min.css");
+		links.Add(path+"/js/i18n/en.min.js");
+		links.Add(path+"/css/wet-boew.min.css");//only found in canada.ca*/
+
+		//Unnecessary for older versions
+		/*if(theme == "GCWeb") {
+			links.Add(path+"/"+theme+"/css/social-media-centre.min.css");//only found in canada.ca, only in gcweb
+			links.Add(path+"/"+theme+"/css/mobile-centre.min.css");//only found in canada.ca, only in gcweb
+		}*/
+	
+		//Check:
+		//util-min.css
+		//pe-ap-min.css
+		//theme-min.css
+		//en-min.js
+		//settings,js
+		//theme-min.js
+		//pe-ap-min.js
 		dirName = Path.GetFileName(path);
 		Console.WriteLine(); 
-		XmlDocument doc = new XmlDocument();
 		
-		XmlWriter xmlWriter = XmlWriter.Create("./test.xml");
+		
+		//Have to create new document because other XML readers require Linq libraries 
+		//This is to format XML with white space
+		XmlWriterSettings settings = new XmlWriterSettings();
+		settings.Indent = true;
+		settings.IndentChars = "\t";
+		XmlWriter xmlWriter = XmlWriter.Create(fileLocation, settings);		
 
 		xmlWriter.WriteStartDocument();
 		xmlWriter.WriteStartElement("widgets");
@@ -76,15 +90,15 @@ public static class UpdateXml
 		xmlWriter.WriteStartElement("dir");
 		xmlWriter.WriteAttributeString("name", dirName);
 		
-		for(int i=0; i < links.Length; i++){
-			fileText = File.ReadAllText(@links[i]);
-			fileName = Path.GetFileName(links[i]);
+		foreach(string link in links){
+			fileText = File.ReadAllText(@link);
+			fileName = Path.GetFileName(link);
 			hash = UpdateXml.getHash(fileText);
 			
-				xmlWriter.WriteStartElement("file");
-				xmlWriter.WriteAttributeString("name", fileName);
-				xmlWriter.WriteAttributeString("hash", hash.ToString());
-				xmlWriter.WriteEndElement();
+			xmlWriter.WriteStartElement("file");
+			xmlWriter.WriteAttributeString("name", fileName);
+			xmlWriter.WriteAttributeString("hash", hash.ToString());
+			xmlWriter.WriteEndElement();
 			
 			Console.WriteLine(fileName + ": " + hash);
 		}	
@@ -101,13 +115,13 @@ class RunUpdate
 	static void Main()
 	{
 		string path; 		
-		path = @"C:\Projects\hashxml\cdn_folders\themes-dist-4.0.16-gcweb\themes-dist-4.0.16-gcweb";
+		path = @"C:\Projects\hashxml\cdn_folders\themes-dist-4.0.21-gcweb\themes-dist-4.0.21-gcweb";
 		UpdateXml.writeHash("GCWeb", path);
 
-		//path = @"C:\Projects\hashxml\cdn_folders\themes-dist-4.0.16-theme-gc-intranet\themes-dist-4.0.16-theme-gc-intranet";
+		//path = @"C:\Projects\hashxml\cdn_folders\themes-dist-4.0.21-theme-gc-intranet\themes-dist-4.0.21-theme-gc-intranet";
 		//UpdateXml.writeHash("theme-gc-intranet", path);
 		
-		//path = @"C:\Projects\hashxml\cdn_folders\themes-dist-4.0.16-theme-gcwu-fegc\themes-dist-4.0.16-theme-gcwu-fegc";
+		//path = @"C:\Projects\hashxml\cdn_folders\themes-dist-4.0.21-theme-gcwu-fegc\themes-dist-4.0.21-theme-gcwu-fegc";
 		//UpdateXml.writeHash("theme-gcwu-fegc", path);
 		
 
